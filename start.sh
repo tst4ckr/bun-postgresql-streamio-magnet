c9eeff7c126c81f34c8ba41f53878b4169665296
tst4ckr <tst4ckr@users.noreply.github.com>
2025-09-07 00:20:40 -0500

    fix(start.sh): run bun as root to fix permission issues on Windows mounts
    The change was made because running as appuser was causing permission problems with Windows volume mounts. Running as root resolves these issues while maintaining functionality.

--- start.sh
+++ start.sh
@@ -7,5 +7,5 @@ gosu debian-tor /usr/bin/tor -f /etc/tor/torrc &
 # Esperar a que Tor se inicialice
 sleep 5
 
-# Ejecutar la aplicación bun como appuser
-exec gosu appuser bun run start
+# Ejecutar la aplicación bun como root para evitar problemas de permisos en montajes de Windows
+exec bun run start
